7ddae58451d596cf06b2e51370f07d75e3b3886d
pica pau <meugit@outlook.com>
2019-03-02 21:28:43 -0300

    MAis alterações - adicioando questões.

--- db/schema.rb
+++ db/schema.rb
@@ -33,18 +33,6 @@ ActiveRecord::Schema.define(version: 2019_02_14_194340) do
     t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
-  create_table "profiles", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_profiles_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_profiles_on_reset_password_token", unique: true
-  end
-
   create_table "questions", force: :cascade do |t|
     t.text "description", null: false
     t.integer "subject_id"
